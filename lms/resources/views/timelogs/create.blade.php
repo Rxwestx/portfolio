37c9aa8d470fc56048f38c4796db7cdf129c45af
Rxwestx <wexriexst13@gmail.com>
2026-01-12 19:45:38 +0900

    Modify_File

--- lms/resources/views/timelogs/create.blade.php
+++ lms/resources/views/timelogs/create.blade.php
@@ -14,19 +14,19 @@
             @csrf
             <div class="mt-8">
                 <div class="w-full">
-                    <label for="study_date" class="font-semibold mt-4">学習日</label>
+                    <label for="study_date" class="font-semibold mt-4">学習日:</label>
                     <x-input-error :messages="$errors->get('study_date')" class="mt-2" />
                     <input type="date" name="study_date" class="w-auto py-2 border border-gray-300 rounded-md"
                         id="study_date" value="{{ old('study_date', now()->format('Y-m-d')) }}">
                 </div>
 
-                <div class="w-full flex flex-col">
-                    <label for="duration_minutes" class="font-semibold mt-4">学習時間（分）</label>
+                <div class="w-full mt-8">
+                    <label for="duration_minutes" class="font-semibold mt-4">学習時間（分）:</label>
                     <x-input-error :messages="$errors->get('duration_minutes')" class="mt-2" />
-                    <input type="number" name="duration_minutes" class="w-auto py-2 border border-gray-300 rounded-md"
+                    <input type="number" name="duration_minutes" class="w-24 py-2 border border-gray-300 rounded-md"
                         id="duration_minutes" min="1" value="{{ old('duration_minutes') }}">
                 </div>
-                <x-primary-button class="mt-4">
+                <x-primary-button class="mt-4 text-center">
                     報告する
                 </x-primary-button>
         </form>
