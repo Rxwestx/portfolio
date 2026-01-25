364c763a0751b3353c0eef0632337b1ab191d967
Rxwestx <wexriexst13@gmail.com>
2026-01-25 14:49:07 +0900

    update_style:button

--- lms/resources/views/components/primary-button.blade.php
+++ lms/resources/views/components/primary-button.blade.php
@@ -1,3 +1,4 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-amber-300 border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-400 focus:bg-yellow-500 active:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blade-main border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-blade-dark focus:bg-blade-dark active:bg-blade-dark focus:outline-none focus:ring-2 focus:ring-blade-dark focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
