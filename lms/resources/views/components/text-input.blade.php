fdb40b46e1dee1c3acab29e8467d70521a33a9f4
Rxwestx <wexriexst13@gmail.com>
2026-01-25 22:15:55 +0900

    update:input_form

--- lms/resources/views/components/text-input.blade.php
+++ lms/resources/views/components/text-input.blade.php
@@ -3,5 +3,5 @@
 <input @disabled($disabled)
     {{ $attributes->merge([
         'class' =>
-            'border-gray-300 focus:border-blade-dark focus:ring-2 focus:ring-offset-1 focus:ring-blade-dark focus:outline-none rounded-md shadow-sm',
+            'border-gray-300 focus:border-blade-dark focus:ring-2 focus:ring-offset-1 focus:ring-blade-dark focus:outline-none rounded-md shadow-sm bg-blade-neon text-gray-600' ,
     ]) }}>
