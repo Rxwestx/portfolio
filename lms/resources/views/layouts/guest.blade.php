f741dc4d1c93f28acfee2c0ce8fb3d41ebc8fb6c
Rxwestx <wexriexst13@gmail.com>
2026-01-26 23:48:13 +0900

    style:class

--- lms/resources/views/layouts/guest.blade.php
+++ lms/resources/views/layouts/guest.blade.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500&display=swap" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="m-plus-1p-light antialiased bg-blade-pale">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-            <div>
-                <a href="/">
-                    <x-application-logo width="100" class="text-gray-500" />
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500&display=swap" rel="stylesheet">
 
-            <div class="w-full max-w-md mt-6 px-6 py-4 bg-blade-neon shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="m-plus-1p-light antialiased bg-blade-pale">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+        <div>
+            <a href="/">
+                <x-application-logo class="!w-32 text-gray-500" />
+            </a>
         </div>
-    </body>
+
+        <div class="w-full max-w-md mt-6 px-6 py-4 bg-blade-neon shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
+    </div>
+</body>
+
 </html>
--- lms/resources/views/timelogs/index.blade.php
+++ lms/resources/views/timelogs/index.blade.php
@@ -4,21 +4,21 @@
             学習記録一覧
         </h2>
     </x-slot>
-    <div class="mx-auto px-6">
+    <div class="mt-16 max-w-md px-6">
         <x-message :message="session('message')" />
         @foreach ($timeLogs as $timeLog)
-            <div class="mt-4 p-8 bg-white w-full rounded-2xl">
-                <h1 class="p-4 text-lg font-semibold">
+            <div class="mt-4 p-8 bg-blade-neon w-full rounded-2xl shadow-md ">
+                <h1 class="p-4 text-lg font-2xl text-gray-600">
                     学習日：
-                    <a href="{{ route('timelogs.show', $timeLog->id) }}" class="text-blue-600">
+                    <a href="{{ route('timelogs.show', $timeLog->id) }}" class="text-blade-dark">
                         {{ $timeLog->logged_at->format('Y年m月d日') }}
                     </a>
                 </h1>
                 <hr class="w-full">
-                <p class="mt-4 p-4">
+                <p class="mt-4 p-4 text-gray-600">
                     学習時間：{{ $timeLog->duration_minutes }} 分
                 </p>
-                <div class="p-4 text-sm font-semibold">
+                <div class="p-4 text-sm test-xl text-gray-600">
                     <p>
                         記録日時：{{ $timeLog->created_at->format('Y-m-d H:i') }}
                     </p>
