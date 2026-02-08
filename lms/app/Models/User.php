2bbd8d3d99eddf3c7ab973b698a25c04cac16d2b
Rxwestx <wexriexst13@gmail.com>
2026-02-08 17:15:09 +0900

    add:Relation

--- lms/app/Models/User.php
+++ lms/app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Models\Goal;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -44,6 +44,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function goals(){
+        return $this->hasMany(Goal::class);
     }
     public function character(){
         return $this->hasOne(Character::class);
