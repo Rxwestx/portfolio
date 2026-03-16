53ca2eb2b492ea50c0be110e7e717a6d190ac11f
Rxwestx <wexriexst13@gmail.com>
2026-03-17 02:30:46 +0900

    fix: フォーカス時のナビゲーションリンクの背景色を修正

--- Inari-path/resources/views/components/responsive-nav-link.blade.php
+++ Inari-path/resources/views/components/responsive-nav-link.blade.php
@@ -4,7 +4,7 @@
     $classes =
         $active ?? false
             ? 'block w-full ps-3 pe-4 py-2 border-l-4 border-blade-main text-start text-base font-medium text-gray-600 bg-blade-neon focus:outline-none focus:text-gray-500 focus:border-blade-dark transition duration-150 ease-in-out'
-            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-blade-pale hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-blade-main focus:border-gray-300 transition duration-150 ease-in-out';
+            : 'block w-full ps-3 pe-4 py-2 border-l-4 border-transparent text-start text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-blade-pale hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-blade-pale focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
