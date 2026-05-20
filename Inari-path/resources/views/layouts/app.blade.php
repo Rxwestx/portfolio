9f4bee07a067302a320b5aad0465514634ee0194
Rxwestx <wexriexst13@gmail.com>
2026-05-20 18:24:06 +0900

    feat: app.blade.phpにcustom.cssの読み込みを追加

--- Inari-path/resources/views/layouts/app.blade.php
+++ Inari-path/resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300;400;500&display=swap" rel="stylesheet">
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
 </head>
 
 <body class="m-plus-1p-light antialiased bg-blade-pale">
