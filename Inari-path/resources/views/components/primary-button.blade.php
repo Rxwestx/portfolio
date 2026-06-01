100427039619395840224bde8191907e7d4481af
Rxwestx <wexriexst13@gmail.com>
2026-06-01 22:29:56 +0900

    fix: primary-button.blade.phpのスタイルを改善し、一貫性を向上

--- Inari-path/resources/views/components/primary-button.blade.php
+++ Inari-path/resources/views/components/primary-button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-blade-main border border-transparent rounded-full font-semibold text-xs text-white uppercase tracking-widest hover:bg-blade-dark focus:bg-blade-dark active:bg-blade-dark focus:outline-none focus:ring-2 focus:ring-blade-dark focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex min-h-10 items-center justify-center rounded-[5px] border border-transparent bg-blade-main px-4 py-2 text-sm font-semibold text-white transition ease-in-out duration-150 hover:bg-blade-main/90 focus:outline-none focus:ring-2 focus:ring-blade-main focus:ring-offset-2 active:bg-blade-main/90']) }}>
     {{ $slot }}
 </button>
