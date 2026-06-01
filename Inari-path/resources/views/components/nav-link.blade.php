41b5dda83ae93893cdbf8e309f10d72d30bec679
Rxwestx <wexriexst13@gmail.com>
2026-06-01 22:29:44 +0900

    fix: nav-link.blade.phpのスタイルを改善し、一貫性を向上

--- Inari-path/resources/views/components/nav-link.blade.php
+++ Inari-path/resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-blade-dark text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blade-dark transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-700 hover:text-gray-900 hover:border-blade-main focus:outline-none focus:text-gray-900 focus:border-blade-main transition duration-150 ease-in-out';
+            ? 'inline-flex items-center border-b-2 border-blade-main px-1 pt-1 text-sm font-semibold leading-5 text-gray-900 transition duration-150 ease-in-out focus:outline-none focus:border-blade-main'
+            : 'inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium leading-5 text-gray-600 transition duration-150 ease-in-out hover:border-blade-neon hover:text-gray-900 focus:outline-none focus:border-blade-neon focus:text-gray-900';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
